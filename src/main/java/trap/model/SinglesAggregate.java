18a8750cad936f2fd0d9ed39b527f6fd01f19fda
Matt Busche <mrbusche@gmail.com>
2019-05-10 14:04:30 -0500

    fix for singles (#35)

--- src/main/java/trap/model/SinglesAggregate.java
+++ src/main/java/trap/model/SinglesAggregate.java
@@ -2,16 +2,20 @@ package trap.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "singlesaggregate")
 public class SinglesAggregate {
+    @EmbeddedId
+    private SinglesAggregateIdentity singlesAggregateIdentity;
+    @Column(name="team", insertable = false, updatable = false)
     String team;
-    @Id
+    @Column(name="athlete", insertable = false, updatable = false)
     String athlete;
     String classification;
     String gender;
---
+++ src/main/java/trap/model/SinglesAggregateIdentity.java
@@ -0,0 +1,22 @@
+package trap.model;
+
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+@Data
+public class SinglesAggregateIdentity implements Serializable {
+    private String team;
+    private String athlete;
+
+    public SinglesAggregateIdentity() {
+
+    }
+
+    public SinglesAggregateIdentity(String team, String athlete) {
+        this.team = team;
+        this.athlete = athlete;
+    }
+}
