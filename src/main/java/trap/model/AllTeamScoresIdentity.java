2e70a4f5483511ce71162529ecc18aca44022c0a
Matt Busche <mrbusche@gmail.com>
2019-11-01 20:57:34 -0500

    Need a no args constructor for hibernate

--- src/main/java/trap/model/AllTeamScoresIdentity.java
+++ src/main/java/trap/model/AllTeamScoresIdentity.java
@@ -1,17 +1,19 @@
 package trap.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 class AllTeamScoresIdentity implements Serializable {
-    private final String team;
-    private final String classification;
-    private final String athlete;
-    private final String type;
+    private String team;
+    private String classification;
+    private String athlete;
+    private String type;
 }
