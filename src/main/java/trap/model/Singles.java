59868d56390e6f7d496e6b4d49a139dcc3358fd7
Matt Busche <mrbusche@gmail.com>
2020-12-13 22:23:39 -0600

    add entities for tables

---
+++ src/main/java/trap/model/Singles.java
@@ -0,0 +1,39 @@
+package trap.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Table(name = "singles")
+public class Singles {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+    Integer CompId;
+    String EventId;
+    String Event;
+    Integer LocationId;
+    String Location;
+    String EventDate;
+    String SquadName;
+    String Team;
+    String Athlete;
+    Integer AthleteId;
+    String Classification;
+    String Gender;
+    Integer Round1;
+    Integer Round2;
+    Integer Round3;
+    Integer Round4;
+    Integer Round5;
+    Integer Round6;
+    Integer Round7;
+    Integer Round8;
+}
