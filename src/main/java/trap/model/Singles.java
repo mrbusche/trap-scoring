c511dcfc8ff33c1dab30e5a17015a86a36d84596
Matt Busche <mrbusche@gmail.com>
2020-12-14 21:57:33 -0600

    clean up duplicated code

--- src/main/java/trap/model/Singles.java
+++ src/main/java/trap/model/Singles.java
@@ -2,7 +2,6 @@ package trap.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,27 +12,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Data
 @Entity
 @Table(name = "singles")
-public class Singles {
-    Integer compId;
-    String eventId;
-    String event;
-    Integer locationId;
-    String location;
-    String eventDate;
-    String squadName;
-    String team;
-    String athlete;
-    Integer athleteId;
-    String classification;
-    String gender;
-    Integer round1;
-    Integer round2;
-    Integer round3;
-    Integer round4;
-    Integer round5;
-    Integer round6;
-    Integer round7;
-    Integer round8;
+public class Singles extends TrapTypes {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Integer id;
---
+++ src/main/java/trap/model/TrapTypes.java
@@ -0,0 +1,24 @@
+package trap.model;
+
+public class TrapTypes {
+    Integer compId;
+    String eventId;
+    String event;
+    Integer locationId;
+    String location;
+    String eventDate;
+    String squadName;
+    String team;
+    String athlete;
+    Integer athleteId;
+    String classification;
+    String gender;
+    Integer round1;
+    Integer round2;
+    Integer round3;
+    Integer round4;
+    Integer round5;
+    Integer round6;
+    Integer round7;
+    Integer round8;
+}
