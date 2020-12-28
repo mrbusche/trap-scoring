df6e7ef3baa63af6b50bc181534f17ed829383b8
Matt Busche <mrbusche@gmail.com>
2020-12-28 14:57:17 -0600

    Update ReportHelperTest.java

--- src/test/java/trap/report/ReportHelperTest.java
+++ src/test/java/trap/report/ReportHelperTest.java
@@ -1,11 +1,9 @@
 package trap.report;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
