f4e79c826f62002eba1f5a9bffce5271a9f86138
Matt Busche <mrbusche@gmail.com>
2020-12-28 14:06:52 -0600

    add test

---
+++ src/test/java/trap/report/ReportHelperTest.java
@@ -0,0 +1,17 @@
+package trap.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ReportHelperTest {
+
+    ReportHelper reportHelper;
+
+    @Test
+    public void testUpdateDatabase() {
+        assertThat(true).isTrue();
+    }
+}
