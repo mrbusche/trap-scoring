d6e0689a547bc342b23d93398dd1301fe3ee30e1
Matt Busche <mrbusche@gmail.com>
2023-01-04 20:48:01 -0600

    throw better exceptions

--- src/main/java/trap/report/ReportController.java
+++ src/main/java/trap/report/ReportController.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/reports")
 @RequiredArgsConstructor
@@ -13,7 +11,7 @@ public class ReportController {
     private final ReportHelper reportHelper;
 
     @RequestMapping("/export")
-    public String export() throws IOException {
+    public String export() throws Exception {
         reportHelper.doItAll();
         return "Report complete";
     }
