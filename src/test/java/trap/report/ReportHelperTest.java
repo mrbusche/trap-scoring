baeba9c1365521f40cbdbf63be3b821b50f9eb85
Matt Busche <mrbusche@gmail.com>
2023-01-04 20:51:06 -0600

    fix auto formatting

--- src/test/java/trap/report/ReportHelperTest.java
+++ src/test/java/trap/report/ReportHelperTest.java
@@ -14,7 +14,12 @@ class ReportHelperTest {
 
     @Test
     void testFileCreated() {
-        String excelFileName = Stream.of(Objects.requireNonNull(new File(".").listFiles())).filter(file -> !file.isDirectory()).map(File::getName).filter(fileName -> fileName.endsWith(".xlsx")).findFirst().orElseThrow();
+        String excelFileName = Stream.of(Objects.requireNonNull(new File(".").listFiles()))
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .filter(fileName -> fileName.endsWith(".xlsx"))
+                .findFirst()
+                .orElseThrow();
         long fileSize = new File(excelFileName).length();
         assertThat(fileSize).isGreaterThan(125813L);
     }
