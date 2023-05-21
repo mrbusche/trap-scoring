b237860244f704838175685f3e5ad53167852f98
Matt Busche <mrbusche@gmail.com>
2023-05-21 16:18:27 -0500

    run tests

--- src/main/java/trap/report/CommandLineAppStartupRunner.java
+++ src/main/java/trap/report/CommandLineAppStartupRunner.java
@@ -2,8 +2,10 @@ package trap.report;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class CommandLineAppStartupRunner implements CommandLineRunner {
