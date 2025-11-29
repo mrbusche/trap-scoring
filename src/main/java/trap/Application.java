282acee9a2fb0245523ac5d16cedcd6580382768
Matt Busche <mrbusche@gmail.com>
2025-11-28 22:20:50 -0600

    enable virtual threads

--- src/main/java/trap/Application.java
+++ src/main/java/trap/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     static void main(String[] args) {
+        System.setProperty("spring.threads.virtual.enabled", "true");
         SpringApplication.run(Application.class, args).close();
     }
 }
