e97aaae87601616f3156540450f7fbd46f7ffeac
Matt Busche <mrbusche@gmail.com>
2025-11-28 22:44:59 -0600

    Revert "enable virtual threads"
    This reverts commit 282acee9a2fb0245523ac5d16cedcd6580382768.

--- src/main/java/trap/Application.java
+++ src/main/java/trap/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     static void main(String[] args) {
-        System.setProperty("spring.threads.virtual.enabled", "true");
         SpringApplication.run(Application.class, args).close();
     }
 }
