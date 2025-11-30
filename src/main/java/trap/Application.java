d323ef07d5bde261408098cd966a7202396f6723
Matt Busche <mrbusche@gmail.com>
2025-11-29 22:54:42 -0600

    simplify SpringBootApplication

--- src/main/java/trap/Application.java
+++ src/main/java/trap/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-    static void main(String[] args) {
+class Application {
+    void main(String[] args) {
         SpringApplication.run(Application.class, args).close();
     }
 }
